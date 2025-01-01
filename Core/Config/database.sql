0e62ff89eb73ffc8ee70027cbf724b266d4e15dc
Abdelhakim Baalla <abdelhakimbaalla50@gmail.com>
2025-01-01 17:48:26 +0100

    J'ai crée la table des role dans la base de donnée avec deux columns un pour l'id qui est auto incrementez et aussi clé primaire puis le nom de type string

--- Core/Config/database.sql
+++ Core/Config/database.sql
@@ -1,3 +1,9 @@
 -- Creation de base de donnée
 CREATE DATABASE biblioSchool;
 USE biblioSchool;
+
+-- Creation de la table des roles
+CREATE TABLE roles(
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    nom VARCHAR(50)
+);
