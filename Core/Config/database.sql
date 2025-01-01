af97a27ddb17317aedd81d38293a78d2f7b3528d
Abdelhakim Baalla <abdelhakimbaalla50@gmail.com>
2025-01-01 18:19:53 +0100

    J'ai Cree la table  des livres qui est constituer de l'id qui est cle primaire et le nom, auteur, date_creation, prix, seriale de livre , quantité en stock et deux clés etranger tag_id et categorie_id qui se refer a les cle primaire d'autre tableaux

--- Core/Config/database.sql
+++ Core/Config/database.sql
@@ -25,3 +25,18 @@ CREATE TABLE tag(
 	id INT PRIMARY KEY AUTO_INCREMENT,
     nom VARCHAR(20)
 );
+
+-- Creation de la table des livre
+CREATE TABLE livre(
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    nom VARCHAR(150),
+    auteur VARCHAR(30),
+    date_creation DATE,
+    prix DECIMAL(4,2),
+    seriale VARCHAR(10),
+    quantite INT DEFAULT(0),
+    tag_id INT,
+    categorie_id INT,
+    CONSTRAINT FK_tagId FOREIGN KEY (tag_id) REFERENCES tag(id),
+    CONSTRAINT FK_categorieId FOREIGN KEY (categorie_id) REFERENCES categorie(id)
+);
