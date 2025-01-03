0205c93603ac0a78333eed3bd473721197193b3b
Abdelhakim Baalla <abdelhakimbaalla50@gmail.com>
2025-01-03 12:04:55 +0100

    J'ai Crée un noveaux dossier pour les query de sql puis j'ai commencez a crée un class pour l'ajoutement d'un livre qui est constituer des attributs et un constructeur puis les setters

--- Crud/livre/ajouterlivre.php
+++ Crud/livre/ajouterlivre.php
@@ -1,3 +1,43 @@
 <?php
      include("../../core/config/connexion.php");
+
+     class Ajouterlivre{
+        // Attributes
+        private $nom, $auteur, $date_creation, $prix, $seriale, $quantite;
+
+        // Constructor
+        public function __construct($nom, $auteur, $date_creation, $prix, $seriale, $quantite){
+            $this->nom = $nom;
+            $this->auteur = $auteur;
+            $this->date_creation = $date_creation;
+            $this->prix = $prix;
+            $this->seriale = $seriale;
+            $this->quantite = $quantite;
+        }
+
+        // Setters
+        public function setNom($nom){
+            $this->nom = $nom;
+        }
+
+        public function setAuteur($auteur){
+            $this->auteur = $auteur;
+        }
+
+        public function setDateDeCreation($date_creation){
+            $this->date_creation = $date_creation;
+        }
+
+        public function setPrix($prix){
+            $this->prix = $prix;
+        }
+
+        public function setSeriale($seriale){
+            $this->seriale = $seriale;
+        }
+
+        public function setQuantite($quantite){
+            $this->quantite = $quantite;
+        }
+     }
 ?>
---
+++ Query/ajouterlivre.php
@@ -0,0 +1,3 @@
+<?php
+    $queryAjouterLivre = "INSERT INTO livre(nom, auteur, date_creation, prix, seriale, quantite) VALUES('$nom', '$auteur', '$date_creation', $prix, '$seriale', $quantite);";
+?>
