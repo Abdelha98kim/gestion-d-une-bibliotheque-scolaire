0c5ebb80271e8edf0a790cb0c4c9af46dec545f8
Abdelhakim Baalla <abdelhakimbaalla50@gmail.com>
2025-01-03 12:20:29 +0100

    J'ai supprimer l'attribut id puis aussi j'ai supprimer leur parametre dans le constructore car dans la base de donnée l'id il est auto incrementez puis j'ai crée les fonction de set des attribut qui va inisialisez la novelle valeur pour chaque attribut

--- Entities/Administrateur/administrateur.php
+++ Entities/Administrateur/administrateur.php
@@ -1,15 +1,35 @@
 <?php
     class Administrateur{
         // les attributes
-        private $id, $nom, $prenom, $email, $password, $role;
+        private $nom, $prenom, $email, $password, $role;
 
         // Constructore
-        public function __construct($id, $nom, $prenom, $email, $password, $role){
-            $this->id = $id;
+        public function __construct($nom, $prenom, $email, $password, $role){
             $this->nom = $nom;
             $this->prenom = $prenom;
             $this->email = $email;
             $this->password = $password;
+            $this->role = $role;
+        }
+
+        // Setters
+        public function setNom($nom){
+            $this->nom = $nom;
+        }
+
+        public function setPrenom($prenom){
+            $this->prenom = $prenom;
+        }
+
+        public function setEmail($email){
+            $this->email = $email;
+        }
+
+        public function setPassword($password){
+            $this->password = $password;
+        }
+
+        public function setRole($role){
             $this->role = $role;
         }
     }
